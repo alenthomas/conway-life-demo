2e342004408cac8d68c3e25e41ff6e80c6821ee5
Noufal Ibrahim <noufal@nibrahim.net.in>
2015-03-14 17:24:55 +0530

    Implements Board.neighbours for central squares

--- game.py
+++ game.py
@@ -13,4 +13,16 @@ class Board(object):
         else:
             return False
             
+    def neighbours(self, x, y):
+        live = 0
+        dead = 0
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if i == x and j == y:
+                    continue
+                if self.cells[i][j] == 1:
+                    live += 1
+                if self.cells[i][j] == 0:
+                    dead += 1
+        return (live, dead)
     
