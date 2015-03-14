0e54ee1c2a5ad0867e03e9b2e2cd46f54f4e8dac
Noufal Ibrahim <noufal@nibrahim.net.in>
2015-03-14 16:58:00 +0530

    Adds tests for asymmetrical boards

--- test_game.py
+++ test_game.py
@@ -5,3 +5,7 @@ def test_create_board():
     assert b.cells == [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
     b = Board((3,3), [])
     assert b.cells == [[0,0,0], [0,0,0], [0,0,0]]
+    b = Board((3,2), [])
+    assert b.cells == [[0,0], [0,0], [0,0]]
+    b = Board((2,3), [])
+    assert b.cells == [[0,0,0], [0,0,0]]
