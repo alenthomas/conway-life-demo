88e8c330fb01f5d0d0deb049ee1c0d2dcccf244f
Noufal Ibrahim <noufal@nibrahim.net.in>
2015-03-14 17:11:27 +0530

    Adds test for Board.get

--- test_game.py
+++ test_game.py
@@ -14,4 +14,10 @@ def test_create_nonempty_board():
     b = Board((4,4), [[1,1], [3,0]])
     assert b.cells == [[0,0,0,0], [0,1,0,0], [0,0,0,0], [1,0,0,0]]
 
+
+def test_get():
+    b = Board((4,4), [[1,1]])
+    assert b.get(1,1) == True
+    assert b.get(0,0) == False
+
     
