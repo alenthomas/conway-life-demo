baaeddcdf6fa9379e6711dd92cfafff773432975
Noufal Ibrahim <noufal@nibrahim.net.in>
2015-03-14 16:56:40 +0530

    Adds test for one more size

--- test_game.py
+++ test_game.py
@@ -1,4 +1,7 @@
+from game import Board
+
 def test_create_board():
     b = Board((4,4), [])
     assert b.cells == [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
-    
+    b = Board((3,3), [])
+    assert b.cells == [[0,0,0], [0,0,0], [0,0,0]]
