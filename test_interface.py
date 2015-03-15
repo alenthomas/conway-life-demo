2b236ad9edbd46a4d63daf1861fd628c25a444a2
alenthomas <alenthomas9@gmail.com>
2015-03-15 15:13:45 +0530

    Rectifies test for format board

--- test_interface.py
+++ test_interface.py
@@ -6,7 +6,10 @@ def test_parse_input_file():
 
 # to test returned board interface
 def test_format_board():
-     ls = [[0,2], [1,2], [2, 0], [2, 1]]
-     st = ('..*.','..*.','**..','...*')
-     x, y = 4,4
-     assert format_board((x,y), ls) == st
+    ls = [[False, False, False, False],
+          [False, True, False, False],
+          [False, False, True, False],
+          [False, False, False, True]]
+    st = ('....','.*..','..*.','...*')
+    x, y = 4,4
+    assert format_board((x,y), ls) == st
