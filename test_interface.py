6f543751d8ae8e0734a8444aabe3a95f000286e5
alenthomas <alenthomas9@gmail.com>
2015-03-15 14:50:42 +0530

    Adds test for format board

--- test_interface.py
+++ test_interface.py
@@ -1,5 +1,11 @@
 # to test game interface
-from interface import parse_input_file
+from interface import *
 def test_parse_input_file():
     assert parse_input_file("test_input_file.txt") == (
         (4,4),[[0, 0], [1, 1], [2, 2], [3, 1]])
+
+# to test returned board interface
+def test_format_board():
+     ls = [[0,2], [1,2], [2, 0], [2, 1]]
+     st = ('..*.','..*.','**..','...*')
+     assert format_board(ls) == st
