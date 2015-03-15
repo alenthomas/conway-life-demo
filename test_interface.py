e2d3f06c63bc91d710fc1c13c250e387422e30b7
alenthomas <alenthomas9@gmail.com>
2015-03-15 15:45:04 +0530

    Change data for parse_input_file

--- test_interface.py
+++ test_interface.py
@@ -2,7 +2,7 @@
 from interface import *
 def test_parse_input_file():
     assert parse_input_file("test_input_file.txt") == (
-        (4,4),[[0, 0], [1, 1], [2, 2], [3, 1]])
+        (4,4),[[0, 1], [1, 1], [2, 1]])
 
 # to test returned board interface
 def test_format_board():
